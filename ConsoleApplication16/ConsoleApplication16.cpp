ad84fdba7744c6232907eafa3d2e49c85b809aba
Roman Dark <amaii.sakura@gmail.com>
2018-07-16 21:16:29 +0600

    last

--- ConsoleApplication16/ConsoleApplication16.cpp
+++ ConsoleApplication16/ConsoleApplication16.cpp
@@ -3,25 +3,51 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include <time.h>
 
 using namespace std;
 
 int main()
 {
-	int a[9] = { 4, 7, 0, 20, 17, 11, 1, 18, 10 };
+	srand(time(NULL));
+
+	const int x = 12;
+	int a[x];
+
+	for (int i = 0; i < x; i++)
+	{
+		a[i] = -5 + rand() % 15;
+		cout << a[i] << " ";
+	}
+	cout << endl << endl;
 
 	int min_v = INT_MAX, min, j;
+	float sum = 0.0;
 
 	for (int i = 0; i < 9; i++)
 	{
-		for ( j = i; j < 9 ; j++)
+		sum += a[i];
+	}
+
+	cout << "Summa vseh elementov massiva = " << sum << endl;
+
+	float sra = sum / x;
+
+	cout << "Srednee arifmeti4eskoe = " << sra << endl;
+	int n;
+
+	if (sra > 0) n = x * 2 / 3; else n = x * 1 / 3;
+	cout << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		for (j = i; j < n; j++)
 		{
 
 			if (a[j] < min_v) {
 				min_v = a[j];
 				min = j;
 			}
-
 		}
 
 		a[min] = a[i];
@@ -31,6 +57,19 @@ int main()
 
 		min_v = INT_MAX;
 	}
+	cout << "/ ";
+	
+
+	for (int i = n; i <x - (x - n) / 2 ; i++) 
+	{
+		swap(a[i], a[x + n - i - 1]);
+	}
+
+	for (int i = n; i < x; i++)
+	{
+		cout << a[i] << " ";
+	}
+
 	cout << endl;
 
 	return 0;
